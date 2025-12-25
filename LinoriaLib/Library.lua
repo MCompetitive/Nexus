97c428b02e2decaafa74c744ea2c28618fcb735a
Lucent <bmwcompetitive@gmail.com>
2025-12-25 13:34:55 +0000

    Update Library.lua

--- LinoriaLib/Library.lua
+++ LinoriaLib/Library.lua
@@ -8,14 +8,14 @@ local TweenService = game:GetService('TweenService');
 local RenderStepped = RunService.RenderStepped;
 local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
-
+local PlayerGui = player:WaitForChild("PlayerGui")
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
 local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
 
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
-ScreenGui.Parent = gethui();
+ScreenGui.Parent = PlayerGui;
 
 local Toggles = {};
 local Options = {};
